833f8990d9045e0102ae6a04910d8a1e5cd55f6e
Jennifer Moore <jenn.f.moore@gmail.com>
2021-05-13 13:18:30 -0400

    created script for rendering bookdown, and testing rendering. Updated bookdown file

--- script/render_bookdown.R
+++ script/render_bookdown.R
@@ -4,9 +4,15 @@
 #install.packages("bookdown")
 library(bookdown)
 
+#make sure all packages are installed for each markdown before running bookdown
+
+
 
 #render to pdf
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 
 #render to html
 bookdown::render_book("index.Rmd")
+
+
+#after rendering commit/push all files to git
